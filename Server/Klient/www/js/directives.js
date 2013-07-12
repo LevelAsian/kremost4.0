383c69d95182b4d104b6be99963ab477db4384a1
jQrgen <jorgen.notland@gmail.com>
2013-07-12 17:54:30 +0200

    rydder

--- Server/Klient/www/js/directives.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Directives */
-
-
-// Footer --> http://www.blogeek.com.ar/2013/03/23/the-right-way-of-coding-angularjs-how-to-organize-a-regular-webapp/
-
-angular.module('myApp.directives', [])
