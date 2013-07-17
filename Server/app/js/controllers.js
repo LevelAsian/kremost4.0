42af56284972768420f2580f7b089d8a1a09cb09
jQrgen <jorgen.notland@gmail.com>
2013-07-17 11:36:35 +0200

    delete friend og delete status

--- Server/app/js/controllers.js
+++ Server/app/js/controllers.js
@@ -136,7 +136,7 @@ angular.module('myApp.controllers', [])
             })
     })
 
-    .controller('FriendCtrl', function($scope, $routeParams, $http, $rootScope, $route) {
+    .controller('FriendCtrl', function($scope, $routeParams, $http, $rootScope, $route, $location) {
         // stokker bokstavene rundt omkring
         $(function(){
             var container = $(".shuffleletters");
@@ -191,11 +191,20 @@ angular.module('myApp.controllers', [])
             .success(function(){
             })
 
-        $scope.deletestatus = function(status){
+        $scope.friend.currentUserEmail = currentUser.email;
+        $scope.deletefriend = function(){
+            $http.post('/api/deletefriend', $scope.friend)
+                .success(function(){
+                    $location.path('/');
+                });
+        };
 
-        }
+        $http.post('/api/deletestatus/' + $routeParams.email, status)
+            .success(function(){
+                $route.reload();
+            });
 
-        // stokker bokstavene rundt omkring
+// stokker bokstavene rundt omkring
         $(function(){
             var container = $(".shuffleletters");
             setTimeout(function(){
